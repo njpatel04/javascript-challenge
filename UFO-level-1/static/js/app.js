4e750705282563473a274f921aba4fbf27b6e239
njpatel04 <39444799+njpatel04@users.noreply.github.com>
2020-09-11 20:23:39 -0400

    Final homework

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,18 +4,44 @@ var tableData = data;
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
+// adding all data to HTML file
+ tableData.forEach(function(UFOReport) {
+  var row = tbody.append("tr");
 
-
-// Step 3:  Use `Object.entries` to console.log each UFO report value
- data.forEach(function(UFOReport) {
-   console.log(UFOReport);
-   var row = tbody.append("tr");
-
-   Object.entries(UFOReport).forEach(function([key, value]) {
-     console.log(key, value);
+  Object.entries(UFOReport).forEach(function([key, value]) {
      var cell = row.append("td");
      cell.text(value);
    });
- });
+});
 
-// YOUR CODE HERE!
+// button location within HTML file
+var button = d3.select('#filter-btn');
+
+// fuction to perform search when clicked
+button.on('click', function(){
+  
+  //captured the text valued entred by user
+  var input_text = d3.select('.form-control');
+
+  //Read the data value
+  var date = input_text.property('value');
+
+  //Filter the date values that matches users input
+  var data_filter = tableData.filter(UFOReport => UFOReport.datetime === date);
+  
+  var tbody = d3.select('tbody');
+  
+  //reset the table
+  tbody.html("");
+
+  // function to append the data back to HTML file
+  data_filter.forEach(function(UFOReport){
+        var line = tbody.append('tr');
+
+        Object.entries(UFOReport).forEach(function([key,value]){
+            var cell = line.append('td');
+            cell.text(value);
+        })
+  })
+
+});
